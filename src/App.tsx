4f330f0ddbc14c6e619485bc36a295660631d4d7
Carie Fisher <15240149+cehfisher@users.noreply.github.com>
2026-01-28 21:37:21 +0000

    Generated by Spark: the default theme is always dark

--- src/App.tsx
+++ src/App.tsx
@@ -246,10 +246,14 @@ export default function App() {
   }, [])
   
   useEffect(() => {
-    if (darkMode === undefined || darkMode === true) {
-      document.documentElement.classList.add('dark')
-    } else {
+    document.documentElement.classList.add('dark')
+  }, [])
+  
+  useEffect(() => {
+    if (darkMode === false) {
       document.documentElement.classList.remove('dark')
+    } else {
+      document.documentElement.classList.add('dark')
     }
   }, [darkMode])
   
